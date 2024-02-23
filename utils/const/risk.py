1b716e556ab2f2fdf5871fc1e9ee572f887b2ca2
benny <benoitb@tutanota.de>
2024-02-23 23:22:21 +1100

    Add WBETH

--- utils/const/risk.py
+++ utils/const/risk.py
@@ -1,8 +1,9 @@
-from utils.const import CBETH, RETH, SFRXETH, WSTETH
+from utils.const import CBETH, RETH, SFRXETH, WBETH, WSTETH
 
 RISK_REPORTS = {
     WSTETH: "https://docs.prismafinance.com/asset-risk/risk-per-asset/lido-wsteth",
     RETH: "https://docs.prismafinance.com/asset-risk/risk-per-asset/rocketpool-reth",
     SFRXETH: "https://docs.prismafinance.com/asset-risk/risk-per-asset/frax-sfrxeth",
     CBETH: "https://docs.prismafinance.com/asset-risk/risk-per-asset/coinbase-cbeth",
+    WBETH: "https://docs.prismafinance.com/asset-risk/risk-per-asset/binance-wbeth",
 }
